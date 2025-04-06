71455e6066c4b753b220dd538f597b3b5816dbef
hannoobz <hanieffatkhan@gmail.com>
2025-04-07 06:56:38 +0700

    feat: setup initial navigation and action bar

--- app/src/main/java/com/ynt/purrytify/MainActivity.kt
+++ app/src/main/java/com/ynt/purrytify/MainActivity.kt
@@ -3,6 +3,8 @@ package com.ynt.purrytify
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -26,9 +28,12 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_library, R.id.navigation_profile
             )
         )
+        val actionBar : Toolbar = findViewById(R.id.topAppBar)
+        setSupportActionBar(actionBar)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
