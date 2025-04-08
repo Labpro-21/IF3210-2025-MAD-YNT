0eac1650c82fb787de8b078dcfcea1bd1b4156bc
hannoobz <hanieffatkhan@gmail.com>
2025-04-08 18:55:34 +0700

    feat: add song layout

--- app/src/main/java/com/ynt/purrytify/ui/library/LibraryFragment.kt
+++ app/src/main/java/com/ynt/purrytify/ui/library/LibraryFragment.kt
@@ -1,47 +1,76 @@
 package com.ynt.purrytify.ui.library
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SegmentedButton
 import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SheetState
 import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.ynt.purrytify.R
 import com.ynt.purrytify.databinding.FragmentLibraryBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class LibraryFragment : Fragment() {
     private var _binding: FragmentLibraryBinding? = null
@@ -204,37 +233,202 @@ fun LibraryButtons(){
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddSong(setShowPopupSong: (Boolean)->Unit){
-    val title = remember { mutableStateOf("") }
-    val artist = remember { mutableStateOf("") }
+    val title = remember  { mutableStateOf("") }
+    val artist = remember{ mutableStateOf("") }
+    val sheetState = rememberModalBottomSheetState()
+    val coroutineScope = rememberCoroutineScope()
+
     ModalBottomSheet(
-        onDismissRequest = { setShowPopupSong(false)},
+        onDismissRequest = {
+            coroutineScope.launch {
+                sheetState.hide()
+                setShowPopupSong(false)
+            }
+        },
+        sheetState = sheetState,
         containerColor = colorResource(R.color.dark_gray),
-        contentColor = colorResource(R.color.dark_gray)
+        contentColor = colorResource(R.color.dark_gray),
         ){
         Column(
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+
         ){
             Text(
                 text = "Upload Song",
                 fontSize = 16.sp,
                 fontWeight = FontWeight.Medium,
                 color = Color.White,
-                modifier = Modifier.align(Alignment.CenterHorizontally)
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(bottom = 10.dp)
             )
-//            Row {
-//            }
-            //
-//            TextField(
-//                value = title.value,
-//                onValueChange = { title.value = it },
-//                label = { Text("Title") },
-//            )
-//            TextField(
-//                value = artist.value,
-//                onValueChange = { artist.value = it },
-//                label = { Text("Artist") },
-//            )
-            //
+            Row(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(
+                        horizontal = 0.dp
+                    ),
+            ){
+//                Image(
+//                    imageVector = ImageVector.vectorResource(R.drawable.choose_image),
+//                    contentDescription = "Choose Image",
+//                    modifier = Modifier
+//                        .fillMaxWidth(1/2f)
+//                        .padding(horizontal = 0.dp)
+//                )
+                ImagePicker()
+                Spacer(
+                    modifier = Modifier
+                        .width(24.dp)
+                )
+                Image(
+                    imageVector = ImageVector.vectorResource(R.drawable.choose_song),
+                    contentDescription = "Choose Song",
+                    modifier = Modifier
+                        .size(96.dp)
+                )
+            }
+            AddSongTextField(title,"Title",false)
+            AddSongTextField(artist,"Artist",true)
+        }
+        Spacer(
+            modifier = Modifier
+                .height(10.dp)
+        )
+        Row(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(
+                    start = 16.dp,
+                    end = 16.dp,
+                ),
+        ){
+           CancelButton(coroutineScope,sheetState,setShowPopupSong)
+           SaveButton()
         }
     }
+}
+
+
+@Composable
+fun SaveButton(){
+    Button(
+        colors = ButtonDefaults.buttonColors(colorResource(R.color.green)),
+        onClick = {},
+        modifier = Modifier
+            .height(36.dp)
+            .fillMaxWidth(1f)
+            .padding(horizontal = 10.dp)) {
+        Text(
+            text = "Save",
+            color = Color.White
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CancelButton(coroutineScope: CoroutineScope,sheetState: SheetState,setShowPopupSong: (Boolean) -> Unit){
+    Button(
+        colors = ButtonDefaults.buttonColors(colorResource(R.color.medium_dark_gray)),
+        onClick = {
+            coroutineScope.launch {
+                sheetState.hide()
+                setShowPopupSong(false)
+            }
+        },
+        modifier = Modifier
+            .height(36.dp)
+            .fillMaxWidth(1/2f)
+            .padding(horizontal = 10.dp)){
+        Text(
+            text = "Cancel",
+            color = Color.White
+        )
+    }
+
+}
+
+@Composable
+fun AddSongTextField(inputText: MutableState<String>,labelText:String,isLast:Boolean){
+    var insertImeAction = ImeAction.Next
+    if(isLast) insertImeAction = ImeAction.Done
+    OutlinedTextField(
+        singleLine = true,
+        value = inputText.value,
+        onValueChange = { inputText.value = it },
+        label = { Text(text = labelText) },
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = colorResource(R.color.dark_gray),
+            unfocusedContainerColor = colorResource(R.color.dark_gray),
+            disabledContainerColor = colorResource(R.color.dark_gray),
+            focusedLabelColor = colorResource(R.color.green),
+            unfocusedLabelColor = colorResource(R.color.medium_dark_gray),
+            focusedIndicatorColor = colorResource(R.color.green),
+            unfocusedIndicatorColor = colorResource(R.color.medium_dark_gray),
+            focusedTextColor = colorResource(R.color.white),
+            unfocusedTextColor = colorResource(R.color.medium_dark_gray),
+            cursorColor = colorResource(R.color.green)
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp),
+        keyboardOptions = KeyboardOptions(
+            imeAction = insertImeAction
+        )
+    )
+}
+
+@Composable
+fun ImagePicker() {
+    var imageUri = remember { mutableStateOf<Uri?>(null) }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        imageUri.value = uri
+    }
+    Box(
+        modifier = Modifier
+            .size(96.dp)
+            .clickable { launcher.launch("image/*") }
+    ){
+        if (imageUri.value != null) {
+            Image(
+                painter = rememberAsyncImagePainter(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(imageUri.value)
+                        .crossfade(true)
+                        .size(512)
+                        .build()
+                )
+                ,
+                contentDescription = "Selected Image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .aspectRatio(1f)
+                    .size(96.dp)
+                    .border(2.dp, color = colorResource(R.color.medium_dark_gray), shape = RoundedCornerShape(8.dp))
+                    .clip(RoundedCornerShape(8.dp))
+
+            )
+        } else {
+            Image(
+                    imageVector = ImageVector.vectorResource(R.drawable.choose_image),
+                    contentDescription = "Choose Image",
+                    modifier = Modifier
+                        .size(96.dp)
+                        .padding(horizontal = 0.dp)
+                )
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0x000000,
+)
+@Composable
+fun preview(){
+    LibraryLayout()
 }
