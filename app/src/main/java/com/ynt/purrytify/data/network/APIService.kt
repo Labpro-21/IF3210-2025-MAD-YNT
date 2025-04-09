d494018ebd6373c5037f6779b67ab9169639f9b3
unknown <masraflirasid@gmail.com>
2025-04-10 06:14:54 +0700

    feat: setup retrofit for profile

--- app/src/main/java/com/ynt/purrytify/data/network/APIService.kt
+++ app/src/main/java/com/ynt/purrytify/data/network/APIService.kt
@@ -2,11 +2,17 @@ package com.ynt.purrytify.data.network
 
 import com.ynt.purrytify.data.model.LoginRequest
 import com.ynt.purrytify.data.model.LoginResponse
+import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 
-interface AuthService {
+interface APIService {
     @POST("/api/login")
     suspend fun login(@Body request: LoginRequest): Response<LoginResponse>
+
+    @GET("/api/profile")
+    fun getProfile(@Header("Authorization") authHeader : String):
+            Call<ProfileResponse>
 }
