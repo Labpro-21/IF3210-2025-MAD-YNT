98325cf5621849b0ff27eaf78f96682ad3684777
DeltDev <deltenc88@gmail.com>
2025-04-11 19:24:41 +0700

    feat :logout

--- app/src/main/java/com/ynt/purrytify/utils/AuthUtils.kt
+++ app/src/main/java/com/ynt/purrytify/utils/AuthUtils.kt
@@ -1,8 +1,19 @@
 package com.ynt.purrytify.utils
 
 import android.content.Context
+import android.content.Intent
+import com.ynt.purrytify.MainActivity
 
 fun isUserAuthorized(context: Context): Boolean {
     val tokenStorage = TokenStorage(context)
     return !tokenStorage.getAccessToken().isNullOrBlank()
+}
+
+fun logout(context: Context){
+    val tokenStorage = TokenStorage(context)
+    tokenStorage.clearTokens()
+    val intent = Intent(context, MainActivity::class.java).apply {
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+    }
+    context.startActivity(intent)
 }
