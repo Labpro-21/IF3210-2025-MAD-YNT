7cec9afc35b8ac34ed8867aca74879e3489af69b
hannoobz <hanieffatkhan@gmail.com>
2025-05-11 15:37:17 +0700

    fix: missing refresh token on API service

---
+++ app/src/main/java/com/ynt/purrytify/models/RefreshTokenRequest.kt
@@ -0,0 +1,5 @@
+package com.ynt.purrytify.models
+
+data class RefreshTokenRequest(
+    val refreshToken: String
+)
--- app/src/main/java/com/ynt/purrytify/network/APIService.kt
+++ app/src/main/java/com/ynt/purrytify/network/APIService.kt
@@ -3,6 +3,7 @@ package com.ynt.purrytify.network
 import com.ynt.purrytify.models.LoginRequest
 import com.ynt.purrytify.models.LoginResponse
 import com.ynt.purrytify.models.ProfileResponse
+import com.ynt.purrytify.models.RefreshTokenRequest
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -12,6 +13,9 @@ import retrofit2.http.POST
 interface APIService {
     @POST("/api/login")
     suspend fun login(@Body request: LoginRequest): Response<LoginResponse>
+
+    @POST("/api/refresh-token")
+    suspend fun refreshToken(@Body request: RefreshTokenRequest): Response<LoginResponse>
 
     @GET("/api/profile")
     suspend fun getProfile(@Header("Authorization") authHeader : String): Response<ProfileResponse>
